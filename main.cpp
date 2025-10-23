4dc9400b04e3b4decaa41c562dec5fd12ef607f2
ximeninh0 <guiximenes902@gmail.com>
2025-10-23 15:31:10 -0300

    gerando primeira população

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 #include <math.h>
 #include <ctime>
 #include <experimental/random>
-
+#include <random>
+#include <algorithm>
 using namespace std;
 
 class Point
@@ -36,14 +37,15 @@ public:
     }
 };
 
-class City
+class Gene
 {
 private:
     char name;
     Point coordinates;
 
 public:
-    City(char name, int x, int y)
+    Gene(){}
+    Gene(char name, int x, int y)
     {
         set_name(name);
         set_coordinates(x, y);
@@ -73,10 +75,6 @@ public:
     }
 };
 
-class Gene : public City
-{
-};
-
 class Individual
 {
 private:
@@ -85,10 +83,15 @@ private:
     vector<Gene> chromossome;
 
 public:
-    // get_individual_fitness(){}
+    Individual(vector<Gene> new_chromossome, int index, int generation)
+    {
+        this->chromossome = new_chromossome;
+        this->index = index;
+        this->generation = generation;
+    }
     void print_individual()
     {
-        cout << generation << index;
+        cout << 'I' << generation << '_' << index;
         cout << "| ";
         for (Gene gene : chromossome)
         {
@@ -129,6 +132,9 @@ public:
                 chromossome.erase(chromossome.begin() + i);
         }
     }
+    int get_ind_fitness(Individual individuo){
+
+    }
 };
 
 class Population
@@ -138,8 +144,37 @@ private:
     int generation;
     int size_p;
     vector<Individual> individuals;
+    Gene first_city;
 
 public:
+    Population(Gene first_route_city, vector<Gene> cities, int size_p)
+    {
+        this->size_p = size_p;
+        this->first_city = first_route_city;
+
+        vector<Gene> initial_chromossome;
+
+        cities.erase(cities.begin());
+
+        initial_chromossome = cities;
+        int size = initial_chromossome.size();
+        for (int indv_index = 0; indv_index < size_p; indv_index++)
+        {
+            vector<Gene> new_indiv_chromo = initial_chromossome;
+
+            for (int i = 0; i < initial_chromossome.size(); i++)
+            {
+                int random_index = std::experimental::randint(i, size - 1);
+
+                Gene temp = new_indiv_chromo[random_index];
+                new_indiv_chromo[random_index] = new_indiv_chromo[i];
+                new_indiv_chromo[i] = temp;
+            }
+
+            Individual individual(new_indiv_chromo, indv_index + 1, 1);
+            this->individuals.push_back(individual);
+        }
+    }
     void print_population()
     {
         for (Individual individual : individuals)
@@ -180,13 +215,34 @@ public:
 class GeneticEstrategy
 {
 private:
+    Reproduction reproducion_tools;
+    Mutation mutation_tools;
+    PopulationAdjust pop_adj_tools;
+
 public:
+    Individual ApplyGeneticAlgorithim(Population population, int epochs)
+    {
+        Individual solution;
+
+        for(int i = 0; i < epochs ; i++){
+            reproducion_tools.reproduction_logic(Population population);
+        }
+
+        return solution;
+    }
+
+    vector<float> GenerateRouletteArray(Population population){
+        // for()
+    }
 };
 
 class Reproduction
 {
 private:
 public:
+    void reproduction_logic(){
+
+    }
 };
 
 class Mutation
@@ -215,14 +271,14 @@ public:
     {
         cout << "------------------------------ \n"
              << "-----{Algoritmo Genético}----- \n"
-             << "-----{Caixeiro Viajante }----- \n"
+             << "-----{Caixeiro  Viajante}----- \n"
              << "------------------------------ \n"
              << "[1] Gerar pontos aleatórios\n"
-             << "[2] Gerar pontos em cículo (benchmark)\n"
+             << "[2] Gerar pontos em círculo (benchmark)\n"
              << endl;
     }
 
-    vector<City> PopulateCitiesWithRandomPoints(vector<City> cities)
+    vector<Gene> PopulateCitiesWithRandomPoints(vector<Gene> cities)
     {
         std::srand(std::time(nullptr));
         char alfabeto[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
@@ -248,8 +304,8 @@ public:
             } while (pow(random_x, 2) + pow(random_y, 2) >= pow(area, 2));
 
             char name = alfabeto[i];
-            City new_city(name, random_x, random_y);
-            new_city.print_gene();
+            Gene new_city(name, random_x, random_y);
+            // new_city.print_gene();
             cities.push_back(new_city);
         }
         return cities;
@@ -259,23 +315,27 @@ public:
 int main()
 {
     int option;
-    vector<City> cities;
+    vector<Gene> cities;
+    vector<Gene> solution;
     Tools tools;
-
     tools.header();
 
-    do
+
+    // do
+    // {
+    cin >> option;
+    switch (option)
     {
-        cin >> option;
-        switch (option)
-        {
-        case 1:
-            cities = tools.PopulateCitiesWithRandomPoints(cities);
-            break;
-        case 2:
-            for (City city : cities)
-                city.print_gene();
-            break;
-        }
-    } while (option != 3);
+    case 1:
+        cities = tools.PopulateCitiesWithRandomPoints(cities);
+        break;
+        //360 divido pela quantidade de pontos
+    case 2:
+        for (Gene city : cities)
+            city.print_gene();
+        break;
+    }
+    Population population(cities[0], cities, 10);
+    population.print_population();
+    // } while (option != 3);
 }
